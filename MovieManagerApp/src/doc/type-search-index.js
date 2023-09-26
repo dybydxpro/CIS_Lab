7dc15143b1ccae66424d03534f3e6b9a89e3b954
Tharindu Theekshana Dayananda <tharindutd1998@gmail.com>
2023-09-26 23:19:00 +0530

    Commit Documentation.

---
+++ MovieManagerApp/src/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"moviemanager","l":"Customer"},{"p":"moviemanager","l":"Movie"},{"p":"moviemanager","l":"Rental"}];updateSearchResults();
